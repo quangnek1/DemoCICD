66443401197b2d8f02b8ff6ae002d926eba38653
quangnek1 <leduyquang1201@gmail.com>
2026-05-08 14:33:58 +0700

    AddSerilog_Exceptions

--- src/DemoCICD.API/Program.cs
+++ src/DemoCICD.API/Program.cs
@@ -1,47 +1,57 @@
 using DemoCICD.Application.DependencyInjection.Extensions;
+using DemoCICD.Contract.Logging;
 using DemoCICD.Persistence.DependencyInjection.Extensions;
 using DemoCICD.Persistence.DependencyInjection.Options;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog(Serilogger.Configure);
+Log.Information(messageTemplate: $"Starting {builder.Environment.EnvironmentName}...");
 
-builder.Services.AddSwaggerGen();
-
-
-Log.Logger = new LoggerConfiguration().ReadFrom
-    .Configuration(builder.Configuration)
-    .CreateLogger();
-
-builder.Logging
-    .ClearProviders()
-    .AddSerilog();
-
-builder.Host.UseSerilog();
-
-//Add Configuration
-builder.Services.AddConfigureMediaR();
-builder.Services.ConfigureSqlServerRetryOptions(builder.Configuration.GetSection(nameof(SqlServerRetryOptions)));
-builder.Services.AddSqlConfiguration();
-builder.Services.AddRepositoryBaseConfiguration();
-builder.Services.AddConfigurationAutoMapper();
-
-// Api
-builder.Services.AddControllers()
-    .AddApplicationPart(DemoCICD.Presentation.AssemblyReference.Assembly);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddSwaggerGen();
+
+    //Add Configuration
+    builder.Services.AddConfigureMediaR();
+    builder.Services.ConfigureSqlServerRetryOptions(builder.Configuration.GetSection(nameof(SqlServerRetryOptions)));
+    builder.Services.AddSqlConfiguration();
+    builder.Services.AddRepositoryBaseConfiguration();
+    builder.Services.AddConfigurationAutoMapper();
+
+    // Api
+    builder.Services.AddControllers()
+        .AddApplicationPart(DemoCICD.Presentation.AssemblyReference.Assembly);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    //app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+        throw;
+
+    Log.Fatal(ex, messageTemplate: "Unhandled exception");
+}
+finally
+{
+    Log.Information(messageTemplate: $"Shut down {builder.Environment.EnvironmentName} complete");
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
